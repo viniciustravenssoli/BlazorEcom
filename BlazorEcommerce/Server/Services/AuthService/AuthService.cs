ba094792b4e5faadb6f2aa050254a301036b3e10
vts1978 <vinicioussouza@gmail.com>
2023-11-17 15:34:41 -0300

    Adding login in Server

--- BlazorEcommerce/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,7 @@
 ﻿using BlazorEcommerce.Server.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace BlazorEcommerce.Server.Services.AuthService
@@ -6,10 +9,37 @@ namespace BlazorEcommerce.Server.Services.AuthService
     public class AuthService : IAuthService
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
 
-        public AuthService(DataContext context)
+        public AuthService(DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
+            if (user == null)
+            {
+                response.Data = null;
+                response.Success = false;
+                response.Message = "User not found.";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Data = null;
+                response.Success = false;
+                response.Message = "Wrong password.";
+            }
+            else
+            {
+                response.Data = CreateToken(user);
+            }
+
+            return response;
         }
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
@@ -33,7 +63,8 @@ namespace BlazorEcommerce.Server.Services.AuthService
 
             return new ServiceResponse<int>
             { 
-                Data = user.Id
+                Data = user.Id,
+                Message = "User Created with sucess"
             };
 
         }
@@ -53,5 +84,39 @@ namespace BlazorEcommerce.Server.Services.AuthService
                     .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var cumputedHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return cumputedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>();
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString());
+                new Claim(ClaimTypes.Name, user.Email);
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }   
     }
 }
